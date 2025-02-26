a936b32d15d25972419f3657fc9c46e57bd4cb9b
naut54 <edum77@icloud.com>
2025-02-26 13:17:33 +0100

    feat: Add initial project setup and display an image
    This commit introduces the initial project setup with Git integration and sets up the project's inspection profiles.
    It modifies the `MainActivity` to display an image instead of a greeting text.
    - Added `.idea/vcs.xml` for Git version control integration.
    - Added `.idea/inspectionProfiles/profiles_settings.xml` to define inspection profiles.
    - Added `.idea/inspectionProfiles/Project_Default.xml` to configure inspection settings.
    - Modified `MainActivity.kt` to remove the previous `Greeting` function and preview.
    - Replaced the greeting with an image using `Image` composable.
    - Added `gradle.xml` and modified `misc.xml` for project settings.

--- app/src/main/java/com/edumdev/codeviewer/MainActivity.kt
+++ app/src/main/java/com/edumdev/codeviewer/MainActivity.kt
@@ -1,29 +1,31 @@
 package com.edumdev.codeviewer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edumdev.codeviewer.ui.theme.CodeViewerTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CodeViewerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Greeting()
                 }
             }
         }
@@ -31,17 +33,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hell $name!",
-        modifier = modifier
+fun Greeting() {
+    Image(
+        painter = painterResource(id = R.drawable.android),
+        contentDescription = "Descripcion"
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CodeViewerTheme {
-        Greeting("Android")
-    }
 }
